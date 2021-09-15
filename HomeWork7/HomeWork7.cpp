79440a5544be05424fa2148b1cce207ad69b4cb7
TexHexx <prior_it@mail.ru>
2021-09-15 17:37:16 +0300

    To check

--- HomeWork7/HomeWork7.cpp
+++ HomeWork7/HomeWork7.cpp
@@ -1,6 +1,40 @@
-#include <iostream>
+#define _CRT_SECURE_NO_WARNINGS
+#include "Date.h"
+
+void FirstTask() {
+	time_t local_time = time(0);
+	std::unique_ptr<Date> date = nullptr;
+	std::unique_ptr<Date> today = std::make_unique<Date>(localtime(&local_time)->tm_mday, localtime(&local_time)->tm_mon + 1, localtime(&local_time)->tm_year + 1900);
+	std::cout << "Today manual cout: " << today->getDay() << "." << today->getMonth() << "." << today->getYear() << std::endl;
+	std::cout << "Today internal cout: " << *today << std::endl;
+	date = std::move(today);
+
+	if(today == nullptr)
+		std::cout << "Today is nullptr now" << std::endl;
+	else
+		std::cout << "Today internal cout: " << *today << std::endl;
+	
+	if (date == nullptr)
+		std::cout << "Date is nullptr now" << std::endl;
+	else
+		std::cout << "Date internal cout: " << *date << std::endl;
+};
+
+void SecondTask() {
+	std::unique_ptr<Date> date1 = std::make_unique<Date>(15,9,2014);;
+	std::unique_ptr<Date> date2 = std::make_unique<Date>(8,1,2012);
+	std::cout << "Date1: " << *date1 << std::endl;
+	std::cout << "Date2: " << *date2 << std::endl;
+	std::cout << "Bigger date: " << *findBiggerDate(date1, date2) << std::endl;
+	exchangeDatePointers(date1, date2);
+	std::cout << "Date1: " << *date1 << std::endl;
+	std::cout << "Date2: " << *date2 << std::endl;
+};
+
 
 int main()
 {
-
+	FirstTask();
+	SecondTask();
+	return 0;
 }
